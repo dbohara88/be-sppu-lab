17f475127ecf61cc438877639533cf2878a3a597
Deepak Bohara <65617245+dbohara88@users.noreply.github.com>
2022-11-20 18:21:03 +0530

    Update fibonacci.cpp

--- be-1/daa/fibonacci.cpp
+++ be-1/daa/fibonacci.cpp
@@ -1,16 +1,17 @@
 #include <iostream>
 using namespace std;
 
-int fib_recursive(int n) {
-    if(n == 1 || n == 0) 
-    return n;
-    return fib_recursive(n-1) + fib_recursive(n-2);
-}  
+int fibonacciRecursive(int n) {
+    if(n == 0 || n == 1)
+    return 1;
 
-int fib_iterative(int n) {
-    int x = 0, y = 1, z = 0;
-    for(int i=0; i<n; i++) {
-        cout << x <<" ";
+    return fibonacciRecursive(n-1)+fibonacciRecursive(n-2);
+}
+
+int fibonacciIterative(int n) {
+    int x = 1, y = 1, z = 0;
+    for(int i = 0; i < n; i++) {
+        cout << x << " ";
         z = x + y;
         x = y;
         y = z;
@@ -22,15 +23,9 @@ int main()
     int n;
     cin >> n;
 
-    //fibonacci using recursion 
-    // Time Complexity O(2^N)
-    for(int i=0; i<n; i++) 
-    cout << fib_recursive(i) << " ";
+    for(int i=0; i<n; i++)
+    cout << fibonacciRecursive(i) << " ";
     cout << endl;
-
-    //fibonacci using iterative
-    // Time Complexity O(N)
-    fib_iterative(n);
-
+    fibonacciIterative(n);
     return 0;
 }
