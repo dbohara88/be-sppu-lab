bac173964fae658882c03a0ab45a953eeecdc939
Deepak Bohara <65617245+dbohara88@users.noreply.github.com>
2023-05-23 16:08:18 +0530

    Add files via upload

--- be-2/hpc/assignment 2/bubble.cpp
+++ be-2/hpc/assignment 2/bubble.cpp
@@ -1,61 +1,97 @@
-#include<iostream>
-#include<stdlib.h>
-#include<omp.h>
+#include <iostream>
+#include <vector>
+#include <chrono>
+#include <omp.h>
+
 using namespace std;
+using namespace std::chrono;
 
-void bubble(int *, int);
-void swap(int &, int &);
+void parallelBubbleSort(vector<int>& arr) {
+    int n = arr.size();
+    bool swapped;
 
+    #pragma omp parallel
+    {
+        #pragma omp for
+        for (int i = 0; i < n - 1; i++) {
+            swapped = false;
 
-void bubble(int *a, int n)
-{
-    for(  int i = 0;  i < n;  i++ )
-     {  	 
-   	 int first = i % 2; 	 
+            #pragma omp for
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    swap(arr[j], arr[j + 1]);
+                    swapped = true;
+                }
+            }
 
-   	 #pragma omp parallel for shared(a,first)
-   	 for(  int j = first;  j < n-1;  j += 2  )
-   	   {  	 
-   		 if(  a[ j ]  >  a[ j+1 ]  )
-   		  {  	 
-     			 swap(  a[ j ],  a[ j+1 ]  );
-   		  }  	 
-   		   }  	 
-     }
+            if (!swapped) {
+                // If no swap occurred in the inner loop, the array is already sorted
+                break;
+            }
+        }
+    }
 }
 
+void sequentialBubbleSort(vector<int>& arr) {
+    int n = arr.size();
+    bool swapped;
 
-void swap(int &a, int &b)
-{
+    for (int i = 0; i < n - 1; i++) {
+        swapped = false;
 
-    int test;
-    test=a;
-    a=b;
-    b=test;
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                swap(arr[j], arr[j + 1]);
+                swapped = true;
+            }
+        }
 
+        if (!swapped) {
+            // If no swap occurred in the inner loop, the array is already sorted
+            break;
+        }
+    }
 }
 
-int main()
-{
+int main() {
+    int n;
+    cout << "Enter the size of the array: ";
+    cin >> n;
 
-    int *a,n;
-    cout<<"\n enter total no of elements=>";
-    cin>>n;
-    a=new int[n];
-    cout<<"\n enter elements=>";
-    for(int i=0;i<n;i++)
-    {
-   	 cin>>a[i];
-    }
-    
-    bubble(a,n);
-    
-    cout<<"\n sorted array is=>";
-    for(int i=0;i<n;i++)
-    {
-   	 cout<<a[i]<<endl;
+    vector<int> arr(n);
+    cout << "Enter the elements of the array: ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
     }
 
+    // Perform sequential bubble sort
+    vector<int> seqArr = arr;
+    auto startSeq = high_resolution_clock::now();
+    sequentialBubbleSort(seqArr);
+    auto endSeq = high_resolution_clock::now();
+    double seqTime = duration_cast<milliseconds>(endSeq - startSeq).count();
 
-return 0;
+    // Perform parallel bubble sort
+    vector<int> parArr = arr;
+    auto startPar = high_resolution_clock::now();
+    parallelBubbleSort(parArr);
+    auto endPar = high_resolution_clock::now();
+    double parTime = duration_cast<milliseconds>(endPar - startPar).count();
+
+    // Display sorted arrays and performance results
+    cout << "Sequential Bubble Sort Result: ";
+    for (int num : seqArr) {
+        cout << num << " ";
+    }
+    cout << endl;
+    cout << "Sequential Bubble Sort Time: " << seqTime << " milliseconds" << endl;
+
+    cout << "Parallel Bubble Sort Result: ";
+    for (int num : parArr) {
+        cout << num << " ";
+    }
+    cout << endl;
+    cout << "Parallel Bubble Sort Time: " << parTime << " milliseconds" << endl;
+
+    return 0;
 }
